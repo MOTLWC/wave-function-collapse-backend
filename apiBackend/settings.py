1326eaf03a8e51c99bef32418fde0fa504c71f43
FelixMR <felixmcneillrutter@gmail.com>
2024-08-22 15:44:50 +0100

    Added env and database setup

--- apiBackend/settings.py
+++ apiBackend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&t4)nskwz)4jzigisetccz=$2(i=#rnwc1h%q+2*_)%i()=cuz'
+SECRET_KEY = env('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'apiBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wfc_database',
+        'USER': env('USER'),
+        'PASSWORD': env('PASS'),
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
