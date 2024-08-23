840db62efab7aa82425b62bbe1a21a59626e8d76
FelixMR <felixmcneillrutter@gmail.com>
2024-08-23 13:48:43 +0100

    Added exception handler

---
+++ utility/handle_exceptions.py
@@ -0,0 +1,16 @@
+from images.models import Image
+from comments.models import Comment
+from prompts.models import Prompt
+from rest_framework.exceptions import NotFound 
+from rest_framework.response import Response
+
+def handle_exceptions(handler_func):
+    def wrapper(*args, **kwargs):
+        try:
+            return handler_func(*args, **kwargs)
+        except (Comment.DoesNotExist, Image.DoesNotExist, Prompt.DoesNotExist, NotFound) as exc:
+            return Response({"errorMessage":str(exc)})
+        except Exception as exc:
+            print(exc)
+            return Response(exc, 500)
+    return wrapper
